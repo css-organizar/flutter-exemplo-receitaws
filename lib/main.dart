bc628ca54dac781305b2a2f1b4b11dae6add765e
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-07-31 03:35:01 -0300

    Adicionado o Splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:consumo_api_cnpj/applicatioon/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([
+    SystemUiOverlay.bottom,
+    SystemUiOverlay.top,
+  ]);
   return runApp(AppWidget());
 }
