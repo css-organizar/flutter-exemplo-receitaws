f810ab17d8df7b8fdd7fffeedcc4fe3e6901f2a5
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-07-31 02:57:47 -0300

    Ajuste do Reposiory

--- lib/infrastrucure/repository/receitaws_reposiory.dart
+++ lib/infrastrucure/repository/receitaws_reposiory.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
-import 'package:consumo_api_cnpj/applicatioon/commons/constantes/constantes.dart';
+import 'package:consumo_api_cnpj/applicatioon/commons/constantes/receitaWs.dart';
 import 'package:consumo_api_cnpj/domain/entities/empresa.dart';
 import 'package:http/http.dart' as http;
 
-class EmpresaRepository {
+class ReceitaWsRepository {
   Future<Empresa> getCnpjData(String urlStr) async {
     http.Response response;
     var jsonDecode;
